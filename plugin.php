c28d59095e1e6c8b71549c078ac3f8dcbabb3467
Kieran <bmxstreet@gmx.net>
2017-01-24 12:13:55 +0100

    Added Comments

--- plugin.php
+++ plugin.php
@@ -17,16 +17,18 @@ yourls_add_action( 'pre_redirect', 'no_preview_func');
 function no_preview_func( $args ) {
 	$location  = $args[0];
 	$code = $args[1];
+	//test for youtube URL, if not true, use usual method
 	if(substr($location, 0, 23) == "https://www.youtube.com" || substr($location, 0, 16) == "https://youtu.be" ){
     if ($metapreview){
+				//set meta for the minified URL preview
 		      echo '<meta name="description" content="' . $location . '" />
 				        <meta property="og:title" content="Youtube" />
 				        <meta property="og:description" content="' . $location . '" />';
     }
+		//set the redirect to 1 sec so the youtube preview doesn't show
 		echo '<meta http-equiv="refresh" content="1;URL=' . $location . '">';
 	}
 	else{
-		//yourls_do_action( 'pre_redirect', $location, $code );
 		$location = yourls_apply_filter( 'redirect_location', $location, $code );
 		$code     = yourls_apply_filter( 'redirect_code', $code, $location );
 		// Redirect, either properly if possible, or via Javascript otherwise
